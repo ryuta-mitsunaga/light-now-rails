b4a11756f444ffc4cada1b4cc45aead1653762c7
ryuta-mitsunaga <mag03w@yahoo.co.jp>
2024-01-21 15:22:24 +0900

    update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins "localhost:5173","www.lightnoweat.com"
 
     resource "*",
       headers: :any,
